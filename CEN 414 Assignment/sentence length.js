a6eb7db81b3871d910b77690063bfbe5cca5facb
Victor Toba-Ogunleye <114260869+TOV-003@users.noreply.github.com>
2023-01-20 07:53:28 +0100

    Add files via upload

---
+++ CEN 414 Assignment/sentence length.js
@@ -0,0 +1,10 @@
+//finding the length of each sentence 
+var people = ["I am a boy", "I am a girl", "I am a man", "I am a woman",]
+
+document.write(people[0].length);
+
+document.write(people[1].length);
+
+document.write(people[2].length);
+
+document.write(people[3].length);
